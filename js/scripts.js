65f3839d4715de1cbf41eb0c0dace47616c1bc19
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-02 16:16:07 -0700

    add images for each color theme button click and added jumbotron
    Co-Authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $("button#hello").click(function(){
     $("body").removeClass();
     $("body").addClass("blue-background");
+    $("#img3").hide();
+    $("#img2").hide();
+    $("#img1").show();
     $("p#user").text("Hello!");
     $("p#webpage").text("Why hello there!");
   });
@@ -9,6 +12,9 @@ $(document).ready(function(){
   $("button#goodbye").click(function(){
     $("body").removeClass();
     $("body").addClass("white-background");
+    $("#img3").hide();
+    $("#img1").hide();
+    $("#img2").show();
     $("p#user").text("Goodbye!");
     $("p#webpage").text("Goodbye, dear user!");
   });
@@ -16,8 +22,10 @@ $(document).ready(function(){
   $("button#stop").click(function(){
     $("body").removeClass();
     $("body").addClass("red-background");
+    $("#img2").hide();
+    $("#img1").hide();
+    $("#img3").show();
     $("p#user").text("Stop copying me!");
     $("p#webpage").text("Pardon me, I meant no offense.");
   });
-
 });
